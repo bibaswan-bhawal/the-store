2d21ff7f21430a2bee56d5726ce3f92e0044a51b
Bibaswan Bhawal <bibaswan3600@gmail.com>
2021-07-03 06:08:02 +0400

    moved directory data to seperate file

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,38 +1,7 @@
+import DIR_DATA from "./directory.data";
+
 const INTIAL_STATE = {
-    sections: [
-        {
-            title: "Hats",
-            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-            id: 1,
-            linkUrl: "shop/hats",
-        },
-        {
-            title: "Jackets",
-            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-            id: 2,
-            linkUrl: "shop/jackets",
-        },
-        {
-            title: "Sneakers",
-            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-            id: 3,
-            linkUrl: "shop/sneakers",
-        },
-        {
-            title: "Womens",
-            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-            size: "large",
-            id: 4,
-            linkUrl: "shop/womens",
-        },
-        {
-            title: "Mens",
-            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-            size: "large",
-            id: 5,
-            linkUrl: "shop/mens",
-        },
-    ]
+    sections: DIR_DATA
 };
 
 const directoryReducer = (state = INTIAL_STATE, action) => {
