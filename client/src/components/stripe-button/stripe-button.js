24ef8a81ed284af09a3e24a95f624d93e7f26513
Bibaswan Bhawal <bibaswan3600@gmail.com>
2021-08-03 06:39:36 +0400

    added a node JS backend to support stripe payments

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51J9oefGKNo8NIVqM0gKpwpBoY8MiPqZRajZrmPLNf7iZtwce5H8g1wJzKIYDVjnS464jsdrHKDmlNjzvhtqC6YBK00rpjM5XxA';
 
     const onToken = token => {
-        console.log(token);
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('payment Successful')
+        }).catch(error => {
+            console.log('payment error: ', JSON.parse(error));
+            alert('There was an error');
+        }
+        );
     }
 
     return (
---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import stripe from 'stripe';
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
+
+const stripeFunc = stripe(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(__dirname, 'client/build', index.html));
+    });
+}
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('server running on port: ' + port);
+});
+
+app.post('/payment', (request, response) => {
+    const body = {
+        source: request.body.token.id,
+        amount: request.body.amount,
+        currency: 'usd',
+    };
+
+    stripeFunc.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            response.status(500).send({ error: stripeErr });
+        } else {
+            response.status(200).send({ error: stripeRes });
+        }
+    });
+});
